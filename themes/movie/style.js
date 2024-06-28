ad167372e71ba9eb2f6ecc8947ec7a06b24400c0
Klaus <49896487+klaushuanghy@users.noreply.github.com>
2024-06-28 11:04:20 +0800

    Update style.js

--- themes/movie/style.js
+++ themes/movie/style.js
@@ -9,13 +9,13 @@ const Style = () => {
     <style jsx global>{`
       // 底色
       .dark body {
-        background-color: black;
+        background-color: 000000;
       }
 
       .shadow-movie {
         box-shadow:
-          0 26px 58px 0 rgba(0, 0, 0, 0.22),
-          0 5px 14px 0 rgba(0, 0, 0, 0.18);
+          0 0px 0px 0 rgba(0, 0, 0, 0),
+          0 0px 0px 0 rgba(0, 0, 0, 0);
       }
 
       // 视频聚合走马灯
